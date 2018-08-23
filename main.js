473d33e5f00eecc0c45b24561c52842100e427e5
Niraj Nandish <nirajnandish@icloud.com>
2018-08-23 09:49:28 +0400

    Add custom menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { BrowserWindow, app } = require("electron");
+const { BrowserWindow, app, Menu } = require("electron");
 
 function createWindow() {
   window = new BrowserWindow({
@@ -6,6 +6,43 @@ function createWindow() {
     height: 700,
     title: "Test"
   });
+
+  const template = [
+    {
+      label: "File",
+      submenu: [{ role: "quit" }]
+    },
+    {
+      label: "View",
+      submenu: [
+        { role: "reload" },
+        { role: "forceReload" },
+        { type: "separator" },
+        { role: "zoomIn" },
+        { role: "zoomOut" },
+        { role: "resetZoom" },
+        { type: "separator" },
+        { role: "toggleDevTools" },
+        { role: "toggleFullScreen" }
+      ]
+    },
+    {
+      label: "Window",
+      submenu: [
+        { role: "minimize" },
+        {
+          label: "Maximize",
+          click() {
+            window.maximize();
+          }
+        }
+      ]
+    }
+  ];
+
+  let menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
   window.loadFile("index.html");
 }
 
